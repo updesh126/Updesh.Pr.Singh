492d2079709fab89dfd0f43f299e5cc484f75664
Updesh Pr. Singh <updesh071@gmail.com>
2023-04-05 16:34:41 +0530

    inital

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public AudioSource doorSound;
+
+    public bool inReach;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inReach = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorOpen();
+        }
+        else
+        {
+            DoorClose();
+        }
+    }
+
+    void DoorOpen()
+    {
+        Debug.Log("It open");
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+        doorSound.Play();
+    }
+    void DoorClose()
+    {
+        Debug.Log("It close");
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+    }
+}
---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject resume;
+    public GameObject quit;
+
+    public bool on;
+    public bool off;
+
+
+
+
+
+    void Start()
+    {
+        menu.SetActive(false);
+        off = true;
+        on = false;
+    }
+
+
+
+
+    void Update()
+    {
+        if (off && Input.GetButtonDown("Pause"))
+        {
+            Time.timeScale = 0; //nothing will change
+            menu.SetActive(true);
+            off = false;
+            on = true;
+        }
+
+        else if (on && Input.GetButtonDown("Pause"))
+        {
+            Time.timeScale = 1;
+            menu.SetActive(false);
+            off = true;
+            on = false;
+        }
+        
+    }
+
+    public void Resume()
+    {
+            Time.timeScale = 1;
+            menu.SetActive(false);
+            off = true;
+            on = false;
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();// qunit application
+    }
+}
