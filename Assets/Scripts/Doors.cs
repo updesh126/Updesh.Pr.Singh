8c782c23cab547b89d743282802aa6c59ba5697b
Updesh Pr. Singh <updesh071@gmail.com>
2023-04-06 00:21:26 +0530

    🚪 door fixed

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public AudioSource doorSound;
+    public bool isopen;
+    public bool inReach;
+
+
+
+
+    void Start()
+    {
+        inReach = false;
+        isopen = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+
+
+
+
+    void Update()
+    {
+
+        if (!isopen && inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorOpens();
+        }
+
+        else if(isopen && inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorCloses();
+        }
+
+
+
+
+    }
+    void DoorOpens ()
+    {
+        Debug.Log("It Opens");
+        door.SetBool("open", true);
+        door.SetBool("closed", false);
+        doorSound.Play();
+        isopen = true;
+    }
+
+    void DoorCloses()
+    {
+        Debug.Log("It Closes");
+        door.SetBool("open", false);
+        door.SetBool("closed", true);
+        isopen = false;
+    }
+
+
+}
